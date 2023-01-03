13f1d663ee2205b94977fab3398488a369b5d730
Matheus Lopes <matheus.lopesmdev@gmail.com>
2023-01-03 19:53:56 -0300

    Consertos na String

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const { type } = require('os');
 const path = require('path');
 
 const app = express();
 
 // Protocolo HTTP
-const server = require('http').createServer(app); 
+const server = require('http').createServer(app);
 
 // Protocolo Web Socket
 const io = require('socket.io')(server);
@@ -18,16 +19,40 @@ app.use('/', (req, res) => {
     res.render('index.html');
 });
 
-let messages = [];
+let messages;
+const vogais = ["a", "e", "i", "o", "u"];
 
 io.on('connection', socket => {
     console.log(`Socket conectado: ${socket.id}`);
 
-    socket.emit('previousMessages', messages);
-
     socket.on('sendMessage', data => {
-        messages.push(data);
-        socket.broadcast.emit('receivedMessage', data);
+        messages = JSON.stringify(data.message);
+        let tamPalavra = messages.length - 2;
+
+        let palavraInvertida = messages.split("").reverse().join("");
+
+        console.log(messages);
+        console.log(palavraInvertida);
+        console.log(tamPalavra);
+
+        function contaVogal(string) {
+            let count = 0;
+
+            for (let letra of string.toLowerCase()) {
+                if (vogais.includes(letra)) {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        let numVogais = contaVogal(messages);
+        let numConsoantes = tamPalavra - numVogais;
+
+        console.log(numVogais, numConsoantes);
+
+        socket.broadcast.emit('receivedMessage', messages);
     });
 });
 
