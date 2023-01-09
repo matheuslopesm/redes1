1468e9b755a05fa1aa5c6cec5dc1d451800f2eb3
Matheus Lopes <matheus.lopesmdev@gmail.com>
2023-01-09 00:43:34 -0300

    Refação dos sockets + Conclusão do método de inverter string

--- server.js
+++ server.js
@@ -19,43 +19,58 @@ app.use('/', (req, res) => {
     res.render('index.html');
 });
 
-var messages;
-var tamPalavra;
-var palavraInvertida;
-const vogais = ["a", "e", "i", "o", "u"];
+let messages;
+let palavraInvertida;
+// var tamPalavra;
+// const vogais = ["a", "e", "i", "o", "u"];
 
 io.on('connection', socket => {
     console.log(`Socket conectado: ${socket.id}`);
 
-    socket.on('sendMessage', data => {
-        messages = JSON.stringify(data.message);
-        tamPalavra = messages.length - 2;
+    // 2) Servidor recebe a informação, trata e devolve pro Cliente: 
+    socket.on('messageToInvert', data => {
+        messages = JSON.stringify(data.word);
         palavraInvertida = messages.split("").reverse().join("");
 
-        function contaVogal(string) {
-            let count = 0;
-
-            for (let letra of string.toLowerCase()) {
-                if (vogais.includes(letra)) {
-                    count++;
-                };
-            };
-
-            return count;
-        };
-
-        var numVogais = contaVogal(messages);
-        var numConsoantes = tamPalavra - numVogais;
-
-        messages = {
-            palavraInvertida: palavraInvertida,
-            tamPalavra: tamPalavra,
-            numVogais: numVogais,
-            numConsoantes: numConsoantes,
-        };
-
-        socket.broadcast.emit('receivedMessage', messages.palavraInvertida);
+        socket.emit('receivedMessage', palavraInvertida);
     }); 
+
+    socket.on('letterCounter', data => {
+        messages = JSON.stringify(data.word);
+        tamPalavra = messages.length - 2;       
+
+        socket.emit('counterMessage', tamPalavra);
+    }); 
+
+    //     socket.on('letterCounter', data => {
+    //     messages = JSON.stringify(data.message);
+    //     tamPalavra = messages.length - 2;
+    //     palavraInvertida = messages.split("").reverse().join("");
+
+    //     function contaVogal(string) {
+    //         let count = 0;
+
+    //         for (let letra of string.toLowerCase()) {
+    //             if (vogais.includes(letra)) {
+    //                 count++;
+    //             };
+    //         };
+
+    //         return count;
+    //     };
+
+    //     var numVogais = contaVogal(messages);
+    //     var numConsoantes = tamPalavra - numVogais;
+
+    //     messages = {
+    //         palavraInvertida: palavraInvertida,
+    //         tamPalavra: tamPalavra,
+    //         numVogais: numVogais,
+    //         numConsoantes: numConsoantes,
+    //     };
+
+    //     socket.broadcast.emit('receivedMessage', messages.palavraInvertida);
+    // }); 
 });
 
 server.listen(3000);
